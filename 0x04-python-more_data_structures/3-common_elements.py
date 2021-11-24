b7ae7f09a5a4a407e22d516b81554054a53ec13e
cPicton <pictonchris2@gmail.com>
2021-11-24 18:21:19 +0300

    a function that returns a set of common elements in two sets

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,5 +1,6 @@
-#!/usr/bin/python3
 def common_elements(set_1, set_2):
-    set_1 = []
-    set_2 = []
-list(set(set_1).intersection(set_2))
+    result = []
+    for element in set_1:
+        if element in set_2:
+            result.append(element)
+    return result

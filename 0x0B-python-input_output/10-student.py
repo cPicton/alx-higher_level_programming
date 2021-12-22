80575099bea07eda2a528a6eb48f644ef4f0bfc2
cPicton <pictonchris2@gmail.com>
2021-12-22 12:48:18 +0300

    a class student defining by 9-student.py

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
 """
-Module 10-class_to_json
-Contains function that
-returns dictionary description with simple data structure
-(list, dictionary, dictionary, string)
-for JSON serialization of an object
+10-student module
 """
 
 
-def class_to_json(obj):
-    """Returns dictionary description with simple data structure
-       (list, dictionary, dictionary, string)
-       for JSON serialization of an object
-    Args:
-        obj: python object
-    """
-    return obj.__dict__
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """Initializes the Student instance."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """dictionary representation of a Student instance"""
+        my_dict = dict()
+        if type(attrs) is list and all(type(x) is str for x in attrs):
+            for x in attrs:
+                if x in self.__dict__:
+                    my_dict.update({x: self.__dict__[x]})
+            return my_dict
+        return self.__dict__.copy()

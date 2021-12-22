aefda304bd28b6db5b5ac2f0265fc307e13a3577
cPicton <pictonchris2@gmail.com>
2021-12-22 12:37:21 +0300

    a script that adds all arguments to a Python list, and then save them to a file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,27 @@
 #!/usr/bin/python3
-"""Add all arguments to a Python list and save them to a file."""
-
-
+"""
+7-add_item script
+"""
 import sys
+import os
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+"""
+file script
+"""
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
 
-    try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+arglist = []
+for i in sys.argv:
+    if i == sys.argv[0]:
+        pass
+    else:
+        arglist.append(i)
+
+file = "add_item.json"
+if os.path.exists(file):
+    data = load_from_json_file(file)
+    data = data + arglist
+    save_to_json_file(data, file)
+else:
+    save_to_json_file(arglist, file)

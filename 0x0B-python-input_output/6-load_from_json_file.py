40ac906735f83f8c2e6e8c04a77a42ec5a58ad56
cPicton <pictonchris2@gmail.com>
2021-12-22 12:29:09 +0300

    a function that creates an Object from a “JSON file”:

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
 """
-Module 6-from_json_string
-Contains function that returns python data structure from JSON string
+6-load_from_json_file module
 """
 
 
-def from_json_string(my_str):
-    """Returns python data structure from JSON string
-    Args:
-        my_str: json string representation
-    Return:
-        python object
-    """
-    import json
+import json
 
-    return json.loads(my_str)
+
+def load_from_json_file(filename):
+    """creates an Object from a JSON file"""
+
+    with open(filename) as file:
+        return json.load(file)

04da2ff3350ef8092a00b62fd215e1de53b25b60
cPicton <pictonchris2@gmail.com>
2021-12-22 12:14:05 +0300

    json

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,11 +1,27 @@
-#!/usr/bin/python3
-"""
-Module 3-write_file
-Contains function that writes to text file and returns num chars written
-"""
+# Import Module
+import json
+
+# Create geeks function
 
 
-def write_file(filename="", text=""):
-    """writes to text file and returns num chars written"""
-    with open(filename, mode="w", encoding="utf-8") as c:
-        return(c.write(text))
+def to_json_string(my_obj):
+
+	# Define Variable
+	language = "Python"
+	company = "GeeksForGeeks"
+	Itemid = 1
+	price = 0.00
+
+	# Create Dictionary
+	value = {
+		"language": language,
+		"company": company,
+		"Itemid": Itemid,
+		"price": price
+	}
+
+	# Dictionary to JSON Object using dumps() method
+	# Return JSON Object
+	return json.dumps(value)
+
+        return json.dumps(my_obj)

283280f630261fd6fa8749da014c9c49abe53be8
cPicton <pictonchris2@gmail.com>
2021-12-22 12:50:30 +0300

    a class student defining by 10-student.py

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,33 +1,30 @@
 #!/usr/bin/python3
 """
-Module 11-student
-Contains class Student
-that initializes public instance attributes first_name, last_name, and age,
-and has public method to_json that retrieves its dictionary representation
+11-student module
 """
 
 
-class Student():
-    """
-    Public Attributes:
-        first_name
-        last_name
-        age
-    Public Methods:
-        to_json: retrieves its dictionary representation
-    """
+class Student:
     def __init__(self, first_name, last_name, age):
-        """
-        Initializes student with full name and age
-        """
+        """Initializes the Student instance."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
+    def to_json(self, attrs=None):
+        """dictionary representation of a Student instance"""
+        my_dict = dict()
+        if type(attrs) is list and all(type(x) is str for x in attrs):
+            for x in attrs:
+                if x in self.__dict__:
+                    my_dict.update({x: self.__dict__[x]})
+            return my_dict
+        return self.__dict__.copy()
+
+    def reload_from_json(self, json):
         """
-        Returns dictionary description with simple data structure
-        (list, dictionary, dictionary, string)
-        for JSON serialization of an object
+        Replaces all attributes of the Student instance.
         """
-        return self.__dict__
+
+        for x in json:
+            self.__dict__.update({x: json[x]})

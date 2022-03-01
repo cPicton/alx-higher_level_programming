d79eaeea41d3671cd0cd907f46d1bf5c095d6c7c
cPicton <pictonchris2@gmail.com>
2022-03-01 21:34:06 +0300

    a function that converts a number from base 10 to another base passed as argument

---
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/*
+*function that converts a number from base 10 to another base passed as argument
+*Prototype: exports.converter = function (base)
+*You are not allowed to import any file
+*You are not allowed to declare any new variable (var, let, etc..)
+*/
+exports.converter = function (base) {
+  function conv (number) {
+    return number.toString(base);
+  }
+  return conv;
+};
